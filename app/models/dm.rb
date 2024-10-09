f952e0d6438155fa3be8b3bfbceb03eb0cb2c0e6
EC2 Default User <ec2-user@ip-172-31-12-33.ap-northeast-1.compute.internal>
2024-10-09 11:48:18 +0000

    21

--- app/models/dm.rb
+++ app/models/dm.rb
@@ -1,5 +1,5 @@
 class Dm < ApplicationRecord
-    varidates :message, presence: true, length: {maximum: 140}   #これなに？
+    validates :message, presence: true, length: {maximum: 140}   #これなに？
     belongs_to :user
     belongs_to :dmroom
 end
